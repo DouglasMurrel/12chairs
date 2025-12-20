f130fb47c491d3575b64cfe0bed3bd0e0f698921
kolchin <murrel@yandex.ru>
2025-12-20 12:46:30 +0300

    Обработка чат-бота - чат для мастеров

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route(path: '/', name: 'main')]
+    public function main(Request $request): Response
+    {
+        return new Response('hello world');
+    }
+}
