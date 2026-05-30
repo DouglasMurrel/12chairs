34e7a2177d54f012d97d85e4bcd527a2f6c5577d
kolchin <murrel@yandex.ru>
2026-05-31 01:49:36 +0300

    Список заявок в архиве

--- src/Service/OrderList.php
+++ src/Service/OrderList.php
@@ -2,17 +2,29 @@
 
 namespace App\Service;
 
+use Twig\Environment;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Order;
+
 class OrderList {
+    
+    public function __construct(
+        private EntityManagerInterface $em,
+        private Environment $twig
+    )
+    {
+    }
+    
     public function generateText(): string
     {
-        // Здесь ваша логика генерации текста
-        $data = [
-            'date' => (new \DateTime())->format('Y-m-d H:i:s'),
-            'user' => 'Пользователь',
-            'content' => 'Секретный документ, сгенерированный автоматически.',
-            'random_number' => random_int(1000, 9999)
-        ];
+            $orders = $this->em->getRepository(Order::class)->findBy([], ['id'=>'DESC']);
+            $resultText = $this->render('order_list.html.twig', [
+                'orders' => $orders
+            ])->getContent();
+            if ($resultText == ''){
+                $resultText = 'Заявок пока нет';
+            }
         
-        return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        return $resultText;
     }
 }
