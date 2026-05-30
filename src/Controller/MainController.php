a47c4c5863648ba825e2faf7621c665ed8d5e099
kolchin <murrel@yandex.ru>
2026-05-31 01:36:59 +0300

    Список заявок в архиве

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Routing\Attribute\Route;
 use App\Service\OrderList;
 
 class MainController extends AbstractController
